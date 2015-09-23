9f90d89aa5dba85aae975dbbd5964d31a9d632a3
Jonathan Yates <git@jonathantyates.com>
2015-09-23 11:12:46 -0400

    chore(node): include scss middleware

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,30 @@
 import BrowserSync from 'browser-sync';
+import nodeSass from 'node-sass';
+import url from 'url';
 
 // Create an instance of BrowserSync
 const browserSync = BrowserSync.create();
+
+// SCSS middleware
+function scssMiddleware(request, response, next) {
+  let parsedURL = url.parse(request.url);
+  if (parsedURL.pathname.match(/\.scss$/)) {
+    return nodeSass.render({
+      file: `src/${parsedURL.pathname}`
+    }, (error, result) => {
+      response.setHeader('Content-Type', 'text/css');
+      response.end(result.css);
+    });
+  }
+  next();
+}
 
 browserSync.init({
   injectFileTypes: ['scss'],
   files: ['src/**/*'],
   server: {
     baseDir: ['src'],
-    middleware: [],
+    middleware: [scssMiddleware],
     routes: {
       '/node_modules': 'node_modules',
     }

c54089ef30c5fb28f9d308ef1a30ff33f7ef58ac
Jonathan Yates <git@jonathantyates.com>
2015-09-23 11:12:33 -0400

    feat: initial launch screen

---
+++ src/components/launch-screen/launch-screen.js
@@ -0,0 +1,11 @@
+import {Component, View} from 'angular/annotations';
+
+@Component({selector: 'launch-screen'})
+@View({
+  directives: [],
+  styleUrls: ['components/launch-screen/launch-screen.scss'],
+  templateUrl: 'components/launch-screen/launch-screen.html',
+})
+export class LaunchScreen {
+  constructor() {}
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import {bootstrap} from 'angular/annotations';
+import {LaunchScreen} from 'components/launch-screen/launch-screen.js';
 
+bootstrap(LaunchScreen);
